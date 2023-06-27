58497b5878e5430ade5165b20e8ff5fea8d18519
fukphua123 <sanluong22@gmail.com>
2023-06-27 21:12:09 +0700

    add book route

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -3,9 +3,9 @@ const router = require('express').Router();
 const userRoute = require('./user.route');
 const bookRoute = require('./book.route');
 const orderRoute = require('./order.route');
-
+const billRoute = require('./bill.route');
 router.use('/users', userRoute);
 router.use('/books', bookRoute);
 router.use('/orders', orderRoute);
-
+router.use('/bills',billRoute);
 module.exports = router;
